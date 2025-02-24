07c4aaa0f7bb15be21cb7366dc85b0d940e0956a
Mohamed Ramadan <93547459+MohamedRamad3n@users.noreply.github.com>
2025-02-24 21:22:45 +0200

    Problem1

---
+++ even-odd.js
@@ -0,0 +1,11 @@
+
+function evenOdd(value) {
+    /*  if(value%2 === 0){
+         return "Even"
+     }else{
+         return "Odd"
+     } */
+    return value % 2 === 0 ? "Even" : "Odd"
+}
+console.log(evenOdd(5));
+console.log(evenOdd(4));

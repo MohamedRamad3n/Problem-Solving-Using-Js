74fcc28107797d76840882cda8297a8aef1419e4
Mohamed Ramadan <93547459+MohamedRamad3n@users.noreply.github.com>
2025-03-03 22:24:47 +0200

    invert values of array

---
+++ invertValus.js
@@ -0,0 +1,6 @@
+//Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+
+function invert(array) {
+    return array.map((num) => num * -1);
+}
+console.log(invert([1, 2, 3, 4, 5])); //[-1, -2, -3, -4, -5]

bc8a5b0199382b8040a804065d426cf6133963c4
Mohamed Ramadan <93547459+MohamedRamad3n@users.noreply.github.com>
2025-03-14 14:01:06 +0200

    welcome

---
+++ welcom!.js
@@ -0,0 +1,32 @@
+
+
+//solution
+function greet(language) {
+    let languages = {
+        english: 'Welcome',
+        czech: 'Vitejte',
+        danish: 'Velkomst',
+        dutch: 'Welkom',
+        estonian: 'Tere tulemast',
+        finnish: 'Tervetuloa',
+        flemish: 'Welgekomen',
+        french: 'Bienvenue',
+        german: 'Willkommen',
+        irish: 'Failte',
+        italian: 'Benvenuto',
+        latvian: 'Gaidits',
+        lithuanian: 'Laukiamas',
+        polish: 'Witamy',
+        spanish: 'Bienvenido',
+        swedish: 'Valkommen',
+        welsh: 'Croeso'
+    }
+    /* if(languages.hasOwnProperty(language)) {
+        return languages[language]
+    }else{
+        languages["english"]
+    } */
+    return languages[language] || languages.english
+}
+
+console.log(greet('english')) // 'Welcome';
